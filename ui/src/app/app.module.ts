ed662d8b896cfb9464f599047101cd205ef9319c
ratlord <bennett.daniel@gmail.com>
2023-10-16 20:47:04 -0400

    primeng, primeflex, basic inputText

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextInputComponent } from './text-input/text-input.component';
+
+
+// PrimeNG Modules
+import { InputTextModule } from 'primeng/inputtext';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextInputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ui/src/app/text-input/text-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.scss']
+})
+export class TextInputComponent {
+
+  value!: string | undefined;
+}
